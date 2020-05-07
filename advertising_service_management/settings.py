2a7180b864a35b22038bff62a903a2d10a26b293
Giovani <favero.giovaani@gmail.com>
2020-05-06 22:01:27 -0300

    Updated STATIC_ROOT

--- advertising_service_management/settings.py
+++ advertising_service_management/settings.py
@@ -135,6 +135,7 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y %H:%M:%S']
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]

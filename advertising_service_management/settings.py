2c204544cbf9015db0c7eb0eb44656edf7368664
Giovani <favero.giovaani@gmail.com>
2020-05-06 22:42:30 -0300

    Changed SECRET_KEY

--- advertising_service_management/settings.py
+++ advertising_service_management/settings.py
@@ -21,8 +21,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(zo#px*l1szz2x5fi(zz9gd_ak-9!fn1(*@jh9kkb=#6^@c04n'
-
+# SECRET_KEY = '(zo#px*l1szz2x5fi(zz9gd_ak-9!fn1(*@jh9kkb=#6^@c04n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
